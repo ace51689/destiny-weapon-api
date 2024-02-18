6cf3618d1284b775c586fb8ee966dece8a9b5842
Michael Vitollo <ace51689@gmail.com>
2024-02-18 01:39:35 -0500

    Update watermarks.py
    Formatting, added seasons to the expansions_dictionary, seasons_dictionary, watermark_list, and relevant_watermark_list

--- api/watermarks.py
+++ api/watermarks.py
@@ -1,64 +1,64 @@
 # These dictionaries are used to help identify what season each weapon is from
 expansions_dictionary = {
-  'beyond_light': ['hunt', 'chosen', 'splicer', 'lost', 'anniversary'],
-  'shadowkeep': ['undying', 'dawn', 'worthy', 'arrivals'],
-  'forsaken': ['outlaw', 'forge', 'drifter', 'opulence'],
-  'witch_queen': ['risen', 'haunted']
-}
+      'beyond_light': ['hunt', 'chosen', 'splicer', 'lost', 'anniversary'],
+      'shadowkeep': ['undying', 'dawn', 'worthy', 'arrivals'],
+      'forsaken': ['outlaw', 'forge', 'drifter', 'opulence'],
+      'witch_queen': ['risen', 'haunted', 'plunder', 'seraph'],
+      'lightfall': ['defiance', 'deep', 'witch', 'wish']
+      }
 
 seasons_dictionary = {
-      # haunted
+      '/common/destiny2_content/icons/a2fb48090c8bc0e5785975fab9596ab5.png': 'wish',
+      '/common/destiny2_content/icons/3de52d90db7ee2feb086ef6665b736b6.png': 'witch',
+      '/common/destiny2_content/icons/6026e9d64e8c2b19f302dafb0286897b.png': 'deep',
+      '/common/destiny2_content/icons/31445f1891ce9eb464ed1dcf28f43613.png': 'defiance',
+      '/common/destiny2_content/icons/e775dcb3d47e3d54e0e24fbdb64b5763.png': 'seraph',
+      '/common/destiny2_content/icons/a3923ae7d2376a1c4eb0f1f154da7565.png': 'plunder',
       '/common/destiny2_content/icons/81edbfbf0bacf8e2117c00d1d6115f1b.png': 'haunted',
-      # witch queen
       '/common/destiny2_content/icons/4fe83598190610f122497d22579a1fd9.png': 'risen',
-      # risen
       '/common/destiny2_content/icons/b0406992c49c84bdc5febad94048dc01.png': 'risen',
-      # lost
       '/common/destiny2_content/icons/4368a3e344977c5551407845ede830c2.png': 'lost',
-      # 30th
       '/common/destiny2_content/icons/dd4dd93c5606998595d9e5a06d5bfc9c.png': 'anniversary',
-      # dawning
-      '/common/destiny2_content/icons/d91c738e8179465a165e35f7a249701b.png': 'dawning', 
-      # festival of the lost
+      '/common/destiny2_content/icons/d91c738e8179465a165e35f7a249701b.png': 'dawning',
       '/common/destiny2_content/icons/215100c99216b9c0bd83b9daa50ace45.png': 'festival',
-      # splicer
       '/common/destiny2_content/icons/b07d89064a1fc9a8e061f59b7c747fa5.png': 'splicer',
-      # solstice
       '/common/destiny2_content/icons/5f5bed34dcd062be8302ce27b542dce9.png': 'solstice',
-      # chosen
       '/common/destiny2_content/icons/6a52f7cd9099990157c739a8260babea.png': 'chosen',
-      # beyond light
       '/common/destiny2_content/icons/0aff1f4463f6f44e9863370ab1ce6983.png': 'hunt',
-      # hunt
       '/common/destiny2_content/icons/2347cc2407b51e1debbac020bfcd0224.png': 'hunt',
-      # arrivals
       '/common/destiny2_content/icons/796813aa6cf8afe55aed4efc2f9c609b.png': 'arrivals',
-      # worthy
       '/common/destiny2_content/icons/3d335ddc3ec6668469aae60baad8548d.png': 'worthy',
-      # dawn
       '/common/destiny2_content/icons/9b7e4bbc576fd15fbf44dfa259f8b86a.png': 'dawn',
       '/common/destiny2_content/icons/ac012e11fa8bb032b923ad85e2ffb29c.png': 'dawn',
-      # undying
       '/common/destiny2_content/icons/82a8d6f2b1e4ee14e853d4ffbe031406.png': 'undying',
       '/common/destiny2_content/icons/d4141b2247cf999c73d3dc409f9d00f7.png': 'undying',
-      # shadowkeep
       '/common/destiny2_content/icons/ee3f5bb387298acbdb03c01940701e63.png': 'undying',
       '/common/destiny2_content/icons/8aae1c411642683d341b2c4f16a7130c.png': 'undying',
-      # opulence
       '/common/destiny2_content/icons/69bb11f24279c7a270c6fac3317005b2.png': 'opulence',
       '/common/destiny2_content/icons/785e5a64153cabd5637d68dcccb7fea6.png': 'opulence',
-      # drifter
       '/common/destiny2_content/icons/e4a1a5aaeb9f65cc5276fd4d86499c70.png': 'drifter',
       '/common/destiny2_content/icons/f9110e633634d112cff72a67159e3b12.png': 'drifter',
-      # forge
       '/common/destiny2_content/icons/c23c9ec8709fecad87c26b64f5b2b9f5.png': 'forge',
       '/common/destiny2_content/icons/bbddbe06ab72b61e708afc4fdbe15d95.png': 'forge',
-      # outlaw
       '/common/destiny2_content/icons/02478e165d7d8d2a9f39c2796e7aac12.png': 'outlaw',
       '/common/destiny2_content/icons/0669efb55951e8bc9e99f3989eacc861.png': 'outlaw'
-}
+      }
 
 watermark_list = [
+      # wish
+      '/common/destiny2_content/icons/a2fb48090c8bc0e5785975fab9596ab5.png',
+      # witch
+      '/common/destiny2_content/icons/3de52d90db7ee2feb086ef6665b736b6.png',
+      # deep
+      '/common/destiny2_content/icons/6026e9d64e8c2b19f302dafb0286897b.png',
+      # lightfall
+      '/common/destiny2_content/icons/af00bdcd3e3b89e6e85c1f63ebc0b4e4.png',
+      # defiance
+      '/common/destiny2_content/icons/31445f1891ce9eb464ed1dcf28f43613.png',
+      # seraph
+      '/common/destiny2_content/icons/e775dcb3d47e3d54e0e24fbdb64b5763.png',
+      # plunder
+      '/common/destiny2_content/icons/a3923ae7d2376a1c4eb0f1f154da7565.png',
       # haunted
       '/common/destiny2_content/icons/81edbfbf0bacf8e2117c00d1d6115f1b.png',
       # witch queen
@@ -111,6 +111,20 @@ watermark_list = [
     ]
 
 relevant_watermark_list = [
+      # wish
+      '/common/destiny2_content/icons/a2fb48090c8bc0e5785975fab9596ab5.png',
+      # witch
+      '/common/destiny2_content/icons/3de52d90db7ee2feb086ef6665b736b6.png',
+      # deep
+      '/common/destiny2_content/icons/6026e9d64e8c2b19f302dafb0286897b.png',
+      # lightfall
+      '/common/destiny2_content/icons/af00bdcd3e3b89e6e85c1f63ebc0b4e4.png'
+      # defiance
+      '/common/destiny2_content/icons/31445f1891ce9eb464ed1dcf28f43613.png',
+      # seraph
+      '/common/destiny2_content/icons/e775dcb3d47e3d54e0e24fbdb64b5763.png',
+      # plunder
+      '/common/destiny2_content/icons/a3923ae7d2376a1c4eb0f1f154da7565.png',
       # haunted
       '/common/destiny2_content/icons/81edbfbf0bacf8e2117c00d1d6115f1b.png',
       # witch queen
